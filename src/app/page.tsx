1c194dd71a10ca72148c86566714015b62a49fa5
lwj <lwj1267@naver.com>
2025-04-23 20:28:31 +0900

    experience레이아웃 생성 및  ‘sectionBlock’ 공통 스타일 지정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,8 @@ export default function Home() {
         <p>EXPERIENCE</p>
         <p>PROJECT</p>
       </header>
-      <section className={styles.intro}>
+
+      <section className={`${styles.intro} ${styles.sectionBlock}`}>
         <article className={styles.intro_desc}>
           <h1>A B C D E F G H I J K L M</h1>
           <p>
@@ -19,7 +20,20 @@ export default function Home() {
         <div className={styles.intro_image}>
           스크롤시 펼쳐지는 이미지가들어감
         </div>
-        <article></article>
+      </section>
+      <section className={styles.sectionBlock}>marquee position</section>
+
+      <section className={`${styles.experience} ${styles.sectionBlock}`}>
+        <article className={styles.experience_desc}>
+          <h1>A B C D E F G H I J K L M</h1>
+          <p>
+            Elevating brands through creative digital solutions, proudly
+            representing Lisbon's vibrant innovation.
+          </p>
+        </article>
+        <article className={styles.experience_image}>
+          스크롤박스가들어갈것
+        </article>
       </section>
     </div>
   );
